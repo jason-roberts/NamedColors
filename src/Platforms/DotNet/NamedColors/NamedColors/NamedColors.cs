d4731ef8e0831251eab279bf103a1d8e61cd82b2
Jason Roberts <osjasonroberts@gmail.com>
2014-07-13 18:18:53 +0800

    initial dotnet platform work

--- src/Platforms/DotNet/NamedColors/NamedColors/NamedColors.cs
+++ src/Platforms/DotNet/NamedColors/NamedColors/NamedColors.cs
@@ -1,7 +1,53 @@
-﻿namespace NamedColors
+﻿
+namespace NamedColors
 {
-    public class NamedColors
+    public static class NamedColors
     {
+
+     
+ 
+        public static NamedColor Black
+        {
+            get
+            {
+                return new NamedColor("Black", "000000");
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor HotPink
+        {
+            get
+            {
+                return new NamedColor("HotPink", "FF69B4");
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor Red
+        {
+            get
+            {
+                return new NamedColor("Red", "FF0000");
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor White
+        {
+            get
+            {
+                return new NamedColor("White", "FFFFFF");
+            }
+        }
+
+       
 
     }
 }
---
+++ src/Platforms/DotNet/NamedColors/Tests/NamedColorShould.cs
@@ -0,0 +1,18 @@
+﻿using NamedColors;
+using Xunit;
+
+namespace Tests
+{
+    public class NamedColorShould
+    {
+        [Fact]
+        public void CreateCorrectIndividualRgbValues()
+        {
+            var sut = new NamedColor("x", "010203");
+
+            Assert.Equal(1, sut.Red);
+            Assert.Equal(2, sut.Green);
+            Assert.Equal(3, sut.Blue);
+        }
+    }
+}
