669c784a5035c35ca165135df73431d99c0960af
Jason Roberts <osjasonroberts@gmail.com>
2014-07-17 10:30:28 +0800

    dotnet PCL generator changes

---
+++ src/ProjectionBuilder/GenerateProjections/NamedColors.cs
@@ -0,0 +1,252 @@
+﻿
+namespace NamedColors
+{
+    public static class NamedColors
+    {
+
+     
+ 
+        public static NamedColor Black
+        {
+            get
+            {
+                return new NamedColor("Black", 0, 0, 0);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor HotPink
+        {
+            get
+            {
+                return new NamedColor("HotPink", 255, 105, 180);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor Red
+        {
+            get
+            {
+                return new NamedColor("Red", 255, 0, 0);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor White
+        {
+            get
+            {
+                return new NamedColor("White", 255, 255, 255);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUILightGreen
+        {
+            get
+            {
+                return new NamedColor("MetroUILightGreen", 153, 180, 51);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIGreen
+        {
+            get
+            {
+                return new NamedColor("MetroUIGreen", 0, 163, 0);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIDarkGreen
+        {
+            get
+            {
+                return new NamedColor("MetroUIDarkGreen", 30, 113, 69);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIMagenta
+        {
+            get
+            {
+                return new NamedColor("MetroUIMagenta", 255, 0, 151);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIVividPurple
+        {
+            get
+            {
+                return new NamedColor("MetroUIVividPurple", 159, 0, 167);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIPurple
+        {
+            get
+            {
+                return new NamedColor("MetroUIPurple", 126, 56, 120);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIPalePurple
+        {
+            get
+            {
+                return new NamedColor("MetroUIPalePurple", 96, 60, 186);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIAlmostBlack
+        {
+            get
+            {
+                return new NamedColor("MetroUIAlmostBlack", 29, 29, 29);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUITeal
+        {
+            get
+            {
+                return new NamedColor("MetroUITeal", 0, 171, 169);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIPaleBlue
+        {
+            get
+            {
+                return new NamedColor("MetroUIPaleBlue", 239, 244, 255);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIBlue
+        {
+            get
+            {
+                return new NamedColor("MetroUIBlue", 45, 137, 239);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIDarkBlue
+        {
+            get
+            {
+                return new NamedColor("MetroUIDarkBlue", 43, 87, 151);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIYellow
+        {
+            get
+            {
+                return new NamedColor("MetroUIYellow", 255, 196, 13);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUILightOrange
+        {
+            get
+            {
+                return new NamedColor("MetroUILightOrange", 227, 162, 26);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIDarkOrange
+        {
+            get
+            {
+                return new NamedColor("MetroUIDarkOrange", 218, 83, 44);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIBrightRed
+        {
+            get
+            {
+                return new NamedColor("MetroUIBrightRed", 238, 17, 17);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIDarkRed
+        {
+            get
+            {
+                return new NamedColor("MetroUIDarkRed", 185, 29, 71);
+            }
+        }
+
+       
+     
+ 
+        public static NamedColor MetroUIWhite
+        {
+            get
+            {
+                return new NamedColor("MetroUIWhite", 255, 255, 255);
+            }
+        }
+
+       
+
+    }
+}
+
---
+++ src/ProjectionBuilder/GenerateProjections/PasteAsXmlProxyClasses.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateProjections
+{
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class NamedColors
+    {
+
+        private NamedColorsNamedColor[] namedColorField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("NamedColor")]
+        public NamedColorsNamedColor[] NamedColor
+        {
+            get
+            {
+                return this.namedColorField;
+            }
+            set
+            {
+                this.namedColorField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class NamedColorsNamedColor
+    {
+
+        private string nameField;
+
+        private string rGBField;
+
+        private string[] emotionsField;
+
+        /// <remarks/>
+        public string Name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string RGB
+        {
+            get
+            {
+                return this.rGBField;
+            }
+            set
+            {
+                this.rGBField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayItemAttribute("Emotion", IsNullable = false)]
+        public string[] Emotions
+        {
+            get
+            {
+                return this.emotionsField;
+            }
+            set
+            {
+                this.emotionsField = value;
+            }
+        }
+    }
+
+
+}
